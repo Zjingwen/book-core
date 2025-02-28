1ea682d09c195de78822c4b7e618bb2ce1743ad6
jingwen <309940379@qq.com>
2025-02-28 10:29:40 +0800

    release: change

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 产出读书视频
 
 ffmpeg -i 圣诞快乐，劳伦斯先生.mp3 -ss 00:00:02 -t 00:02:00 -c copy output_audio.mp3
+
+https://github.com/tnfe/FFCreator
--- test/transition.2.test.js
+++ test/transition.2.test.js
@@ -1,10 +1,9 @@
-import fs from "fs";
 import path from "path";
-import { FFCreator, FFVideoAlbum, FFScene } from "ffcreator";
+import { FFCreator, FFVideo, FFScene, FFVideoAlbum, FFText } from "ffcreator";
 
 const cacheDir = path.join(path.resolve(), "cache/");
 const outputDir = path.join(path.resolve(), "output/");
-const output = outputDir + "test.mp4";
+const output = outputDir + "test3.mp4";
 
 const creator = new FFCreator({
   cacheDir, // 缓存目录
@@ -12,8 +11,6 @@ const creator = new FFCreator({
   output, // 输出文件名(FFCreatorCenter中可以不设)
   width: 1920, // 影片宽
   height: 1080, // 影片高
-  //   cover: "a.jpg", // 设置封面
-  //   audioLoop: true, // 音乐循环
   fps: 24, // fps
   threads: 4, // 多线程(伪造)并行渲染
   debug: false, // 开启测试模式
@@ -21,18 +18,34 @@ const creator = new FFCreator({
 });
 
 const scene1 = new FFScene();
-scene1.setBgColor("#00ff00"); // 设置背景色
-scene1.setDuration(2); // 设置停留时长
-scene1.setTransition("Fat", 1.5); // 设置过渡动画(类型, 时间)
+// scene.setBgColor("#fff"); // 设置背景色
+// scene.setDuration(3.5); // 设置停留时长
+
+const fvideo1 = new FFVideo({
+  path: path.join(path.resolve(), "output/test1.mp4"),
+  width: 1920 * 2,
+  height: 1080 * 2,
+  x: 0,
+  y: 0,
+});
+scene1.addChild(fvideo1);
+scene1.setDuration(4); // 设置停留时长
+scene1.setTransition("Fat", 1.5);
 creator.addChild(scene1);
 
 const scene2 = new FFScene();
-scene2.setBgColor("#ff00ff"); // 设置背景色
-scene2.setDuration(2); // 设置停留时长
+scene2.setDuration(68); // 设置停留时长
+const fvideo2 = new FFVideo({
+  path: path.join(path.resolve(), "output/test2.mp4"),
+  width: 1920 * 2,
+  height: 1080 * 2,
+  x: 0,
+  y: 0,
+});
+scene2.addChild(fvideo2);
 creator.addChild(scene2);
 
 creator.start(); // 开始加工
-creator.closeLog(); // 关闭log(包含perf)
 creator.on("start", () => {
   console.log(`FFCreator start`);
 });
